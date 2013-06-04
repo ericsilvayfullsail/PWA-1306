3066fb8cfc2972f8d4a33ca58d564123e11b773e
Lyndon Modomo <lmodomo@fullsail.com>
2013-06-03 21:00:09 -0400

    Creating Student File Area

---
+++ student files/Lecture Activities STUDENT/Wk1/wk1A_lecture_activity_setup/js/main.js
@@ -0,0 +1,61 @@
+// self-executing function
+
+// Alert Hello World
+
+/* 
+	Variables
+
+	DATA TYPES:
+		string
+		number
+		boolean
+		array				[0, 1, 2]
+		function			function(){}
+		undefined
+		null
+
+	Variables: (assignment vs declaration, naming, and literals)
+	
+	Variable Names:
+		can have:  letters, numbers, underscore, or $
+		cannot have spaces
+		cannot begin with a number
+		is case-sensitive
+		should use camelCasing
+		make the variable name meaningful
+*/
+// variables
+console.log('------ variables ----------');
+
+
+// string (definition, concatenation, escape character)
+console.log('------ strings ----------');
+
+
+// numbers (definition, concatenation +, math, numbers & strings)
+console.log('------ numbers ----------');
+
+
+// arrays: (definition, indexing, getter/setter)
+console.log('------ arrays ----------');
+
+
+// conditionals: (definition, indexing, math, if, if-else, if-else if, ternary)
+console.log('------ conditionals ----------');
+	/*
+		a CONDITION is a boolean test between two values, the resulting boolean can be saved 
+
+		comparison operators:
+		>=  <=  !=  !==  !
+		Sort: A-Za-z
+
+		conditional syntax:
+			if (condition) {
+	  			//block of code or‘actions’
+			};
+	*/	
+
+
+// Functions: (definition, whitespace, invocation, arguments, returns)
+console.log('------Functions ----------');
+
