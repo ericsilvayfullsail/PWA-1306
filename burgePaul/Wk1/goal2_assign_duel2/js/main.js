e506e5ca88835df04ad2ab930782276ec2ef4857
Paul Burge <paulburge@fullsail.edu>
2013-06-06 15:08:54 -0400

    added comments

--- burgePaul/Wk1/goal2_assign_duel2/js/main.js
+++ burgePaul/Wk1/goal2_assign_duel2/js/main.js
@@ -1,48 +1,38 @@
 //Paul Burge - PWA - 1306;
-//June 4th, 2013
-//The Duel - Part I
+//June 6th, 2013
+//The Duel - Part II
 
-// var fighter1 = "kabal";
-// var hp1 = 100;
-// var dps1 = 50;
-
-// var fighter2 = "kratos";
-// var hp2 = 100;
-// var dps2 = 50;
-
+//here are the variables turned array for each of the fighters' stats.
 var kabalArray = ["kabal", 100, 50];
 var kratosArray = ["kratos", 100, 50];
 
+//here is the fight function
 var fight = function(){
-
+	//here, i have an alert to pop up before the round starts to show original hp values.
 	alert(" ---ROUND 1---\n\n" + kabalArray[0] + "\'s HP is " + kabalArray[1] + " and " + kratosArray[0] + "\'s HP is " + kratosArray[1]);
-
+	//and here is the loop, iterating 10 times or until a victor is found.
 	for (var i = 0; i < 10; i++) {
-
+		//here is the damage output for each fighter. a number between 25 and 50, for the fighters' dps.
 		console.log("-----damage------");
 		var dph1 = Math.floor((Math.random() * (kabalArray[2] - 25) + 25));
 		console.log("Player 1 damage per hit " + dph1);
 		var dph2 = Math.floor((Math.random() * (kratosArray[2] - 25) + 25));
 		console.log("Player 2 damage per hit " + dph2);
 
-		// console.log("------- hp ------");
-		// var fighter1hp = hp1 - dph2;
+		//here, this variable saves the new hp values after the damage takes place.
 		var kabalhp = kabalArray[1] - dph2;
-		// console.log("Player 1 hp reduced by " + fighter1hp);
 		var kratoshp = kratosArray[1] - dph1;
-		// console.log("Player 2 hp reduced by " + fighter2hp);
 
+		//and here, the original hp values of each fighter gets modified and updated to reflect the damage.
 		console.log("------round " + (i + 1) + "----")
-		// hp1 = fighter1hp;
 		kabalArray[1] = kabalhp;
 		console.log("Player 1 HP " + kabalArray[1]);
-		// hp2 = fighter2hp;
 		kratosArray[1] = kratoshp;
 		console.log("Player 2 HP " + kratosArray[1]);
 
-
+		//and here's the result variable to hold the winner check function.
 		var result = winnerCheck();
-
+		//this alert pops up after each round with updated hp values.
 		if(result === "its a tie"){
 			alert(" ---ROUND "+(i + 2)+"---\n\n" + kabalArray[0] + "\'s HP is " + kabalArray[1] + " and " + kratosArray[0] + "\'s HP is " + kratosArray[1]);
 		}else{
@@ -52,28 +42,21 @@ var fight = function(){
 
 	}
 }
-
+//and here is the winner check function itself with each of the messages to show the victor, if there is one.
 var winnerCheck = function(){
 	if(kabalArray[1] > 0 && kratosArray[1] > 0){
-		// alert(fighter1 + "\'s HP is " + hp1 + ", and " + fighter2 + "\'s HP is " + hp2);
 		result = "its a tie";
 	}
 	else if(kratosArray[1] <= 0){
-		//break;
 		result = kabalArray[0] + " wins!";
 	}
 	else if(kabalArray[1] <= 0){
-		// break;
 		result = kratosArray[0] + " wins!";
 	}
 	else if(kabalArray[1] <= 0 && kratosArray[1] <= 0){
-		// break;
 		result = "No winners. Everyone losses.";
 	};
 	return result;
-};
-
-
-
-
+};	
+//fight!
 fight();
