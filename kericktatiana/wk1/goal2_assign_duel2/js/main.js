70e1295a8dae445ac3e28fab7368fe77e66a5728
Tatianae <Miss_Tati_K@yahoo.com>
2013-06-06 13:22:55 -0400

    pull

--- kericktatiana/wk1/goal2_assign_duel2/js/main.js
+++ kericktatiana/wk1/goal2_assign_duel2/js/main.js
@@ -1,30 +1,38 @@
 //Tatiana Kerick - Goal 2 Duel 2
 
+//creating the fighters, adding their health and damage
 var fighter1 = ["Absinthe Drinker", 100, (Math.round(Math.random() * 15))];
 
 var fighter2 = ["Green Fairy", 100, (Math.round(Math.random() * 9))];
 
+//round counter
 var round = 0;
 
 function fight(){
+	//alert before the game starts
 	alert(fighter1[0] + " health: " + fighter1[1] + " *START* " + fighter2[0] + " health: " + fighter2[1]);
 	
+	//loop that goes through the apponents attacks taking health from one another
 	for(var i = 0; i <= 11; i++){
 		var onePlan = Math.random() * fighter1[2];
 		var twoPlan = Math.random() * fighter2[2];
 	
+		//taking away the health	
 		var oneHit = fighter2[1] - fighter1[2];
 		var twoHit = fighter1[1] - fighter2[2];
 		
+		//health is now equale to what is taken away from the "hits"
 		fighter1[1] = twoHit;
 		fighter2[1] = oneHit;
 		
-		result = winnerCheck()
+		result = winnerCheck() //result is equale to the conditions in the winnerCheck function
 		if(result != null){
+			//if the results aren't eqaule to null, show the results
 			alert(result);
 			break
 		}
 		else{
+			//else, continue with the fighting rounds and show to the results in an alert
 			round++;
 			alert("After the attack: " + fighter1[0] + " health: " + fighter1[1] + " *ROUND " + round + "* " + fighter2[0] + " health: " + fighter2[1]);
 		}
