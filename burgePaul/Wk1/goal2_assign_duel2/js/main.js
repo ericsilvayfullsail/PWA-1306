2abafec51677dd2d8021a25aacbd3776a2cf517a
Paul Burge <paulburge@fullsail.edu>
2013-06-06 13:12:34 -0400

    Fixed folder structure and added comments to Duel Part 1

--- burgePaul/Wk1/goal2_assign_duel2/js/main.js
+++ burgePaul/Wk1/goal2_assign_duel2/js/main.js
@@ -2,7 +2,6 @@
 //June 4th, 2013
 //The Duel - Part I
 
-//here, we have our fighters Kabal and Kratos, their healthpoint values and damage values.
 var fighter1 = "kabal";
 var hp1 = 100;
 var dps1 = 50;
@@ -11,31 +10,30 @@ var fighter2 = "kratos";
 var hp2 = 100;
 var dps2 = 50;
 
-//here's our fight function.
 var fight = function(){
-	//a for loop that iterates 10 times is used to show the rounds.
 	for (var i = 0; i < 10; i++) {
-		//here, the damage for each fighter is calculated. its a random value between their damage values and 25.
+
 		console.log("-----damage------");
 		var dph1 = Math.floor((Math.random() * (dps1 - 25) + 25));
 		console.log("Player 1 damage per hit " + dph1);
 		var dph2 = Math.floor((Math.random() * (dps2 - 25) + 25));
 		console.log("Player 2 damage per hit " + dph2);
 
-		//here, the fighters' new hp values are stored in these variables everytime the loop iterates. 
+		// console.log("------- hp ------");
 		var fighter1hp = hp1 - dph2;
+		// console.log("Player 1 hp reduced by " + fighter1hp);
 		var fighter2hp = hp2 - dph1;
+		// console.log("Player 2 hp reduced by " + fighter2hp);
 
-		//and then the original variable for the fighters' hp values becomes the value from where they take damage.
 		console.log("------round " + (i + 1) + "----")
 		hp1 = fighter1hp;
 		console.log("Player 1 HP " + hp1);
 		hp2 = fighter2hp;
 		console.log("Player 2 HP " + hp2);
 
-		//here, we have the winner check and a variable set to it.
+
 		var result = winnerCheck();
-		//the conditional to dictate the winner, if there is one.
+
 		if(result ===  "its a tie"){
 			alert(fighter1 + "\'s HP is " + hp1 + ", and " + fighter2 + "\'s HP is " + hp2);
 		}else{
@@ -47,21 +45,28 @@ var fight = function(){
 
 	}
 }
-//the winner check function
+
 var winnerCheck = function(){
 	if(hp1 > 0 && hp2 > 0){
+		// alert(fighter1 + "\'s HP is " + hp1 + ", and " + fighter2 + "\'s HP is " + hp2);
 		result = "its a tie";
 	}
 	else if(hp2 <= 0){
+		//break;
 		result = fighter1 + " wins!";
 	}
 	else if(hp1 <= 0){
+		// break;
 		result = fighter2 + " wins!";
 	}
 	else if(hp1 <= 0 && hp2 <= 0){
+		// break;
 		result = "No winners. Everyone losses.";
 	};
 	return result;
 };
+
+
+
 
 fight();
