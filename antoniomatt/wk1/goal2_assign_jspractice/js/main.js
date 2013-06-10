e2f2d06892bc2c3b13ddd29c342ddeb982dd5af3
Matt Antonio <mantonio@fullsail.edu>
2013-06-10 13:20:38 -0400

    pushing jspractice files

---
+++ antoniomatt/wk1/goal2_assign_jspractice/js/main.js
@@ -0,0 +1,16 @@
+/*===============================================
+Matt Antonio
+June 6, 2013
+Goal 2 Assignment JSPractice
+===============================================*/
+
+var numArray = [22, 11, 10];
+
+var avgNumbers = function(numbers) {
+    numbers[0] + numbers[1] + numbers[2] + numbers[3] / numbers.length;
+    return numbers;
+}
+
+avgNumbers();
+console.log(numArray);
+
